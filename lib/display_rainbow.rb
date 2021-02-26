97c93f9ca596644d13139c4337f6199ffb1c8fdc
Robyn Taylor <rhtaylor0715@gmail.com>
2021-02-26 23:05:40 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 # Write your #display_rainbow method here
+
+
 def display_rainbow(colors)
-  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   puts "R: #{colors[0]}"
 end
